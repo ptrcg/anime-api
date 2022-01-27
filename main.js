6736d8b1d94b6a2f07b2ccc8635ed8e890a863f1
ptrcg <patriciaannemarego@gmail.com>
2022-01-28 03:15:00 +0800

    final

--- main.js
+++ main.js
@@ -11,14 +11,26 @@ fetch(api_link)
             response.json().then(function(json) {
                 console.log(json);
                 
-                for(let i = 0; i < 4; i++) {
+                for(let i = 5; i < 9; i++) {
                     let title = 'title' + (i+1);
                     let synopsis = 'synopsis' + (i+1);
                     let banner = 'banner' + (i+1);
+                    let rating = 'rating' + (i+1);
+                    let status = 'status' + (i+1);
+                    let ep_count = 'ep_count' + (i+1);
+                    let end_date = 'end_date' + (i+1);
+                    let bot_title = 'bot_title' + (i+1);
+                    let video = 'video' + (i+1);
 
                     document.getElementById(title).innerHTML = json.data[i].attributes.canonicalTitle;
                     document.getElementById(synopsis).innerHTML = json.data[i].attributes.synopsis;
                     document.getElementById(banner).src = json.data[i].attributes.coverImage.original;
+                    document.getElementById(rating).innerHTML = 'Rating: '+json.data[i].attributes.averageRating;
+                    document.getElementById(status).innerHTML = 'Status: '+json.data[i].attributes.status;
+                    document.getElementById(ep_count).innerHTML = 'Episode Count: '+json.data[i].attributes.episodeCount;
+                    document.getElementById(end_date).innerHTML = 'End Date: '+json.data[i].attributes.endDate;
+                    document.getElementById(bot_title).innerHTML = json.data[i].attributes.canonicalTitle;
+                    document.getElementById(video).src = json.data[i].attributes.posterImage.large;
                 }
 
             });
